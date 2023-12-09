aee54cb8c9d594c17f4ad95afadc5354e8d6ba3f
Ruth Adegoke <93517109+bisiAlgoRuthM@users.noreply.github.com>
2023-12-09 09:46:13 +0100

    added forms.py module

--- djangodelights/main/forms.py
+++ djangodelights/main/forms.py
@@ -1,1 +1,29 @@
 from django import forms
+from .models import Ingredent, MenuItem, RecipeRequirement, Purchase, PurchaseItem
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredent
+        fields =['name','quantity','unit','price_per_unit']
+
+
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = ['title', 'price', 'image', 'ingredients']
+
+class RecipeRequirementForm(forms.ModelForm):
+    class Meta:
+        model = RecipeRequirement
+        fields = ['ingredient', 'quantity', 'menu_item']
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ['created_at', 'total_price', 'menu_items']
+
+class PurchaseItem(forms.ModelForm):
+    class Meta:
+        model = PurchaseItem
+        fields = ['purchase', 'menu_item', 'quantity']
